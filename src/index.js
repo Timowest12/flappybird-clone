e9d8c5bafb0406d4706cbce1dc09162e2303fdec
Timowest12 <timowester@gmail.com>
2021-12-16 22:29:56 +0100

    Add pipe distance randomnes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
    physics: {
      default:'arcade',
      arcade:{
-      gravity:{y:400},
+
       debug:true
      }
    },
@@ -19,23 +19,39 @@
       update
    }
  }
+
+ const VELOCITY = 200
+ let bird = null;
+ let upperPipe = null;
+ let LowerPipe = null;
+
+ const pipeVerticalDistanceRange = [150,250]
+ let pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0],pipeVerticalDistanceRange[1])
+ 
+
+ const flapVelocity = 250
+ const initialBirdPosition = {x:config.width/10,y:config.height/2}
+ 
  function preload(params) {
     this.load.image('sky','assets/sky.png')
     this.load.image('bird','assets/bird.png')
     this.load.image('pipe','assets/pipe.png')
  }
 
- const VELOCITY = 200
- let bird = null;
- let pipe = null;
- const flapVelocity = 250
- const initialBirdPosition = {x:config.width/10,y:config.height/2}
+
 
  function create(params) {
    this.add.image(0,0,'sky').setOrigin(0,0)
    //this.add.image(0,0,'pipe').setOrigin(0,0)
    bird = this.physics.add.sprite(initialBirdPosition.x,initialBirdPosition.y,'bird').setOrigin(0.5,0.5)
-   pipe = this.add.sprite(300,100,'pipe').setOrigin(0,0)
+   bird.body.gravity.y = 400
+
+
+   upperPipe = this.physics.add.sprite(400,100,'pipe').setOrigin(0,1)
+   LowerPipe = this.physics.add.sprite(400,upperPipe.y + pipeVerticalDistance,'pipe').setOrigin(0,0)
+
+   pipe.x = 100
+   pipe.y = pipe.height
   
    this.input.on('pointerdown',flap)
    this.input.keyboard.on('keydown_SPACE',flap)
