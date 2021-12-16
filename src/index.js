9631df071e4ad856b5ce9d165352395003eb2e52
Timowest12 <timowester@gmail.com>
2021-12-16 18:18:55 +0100

    Add config obj

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
+ import phaser from 'phaser'
 
-import Phaser from "phaser";
 
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
-  },
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
 
+ const config = {
+   type: Phaser.AUTO,
+   width:800,
+   height:600,
+   physics: {
+     default:'arcade'
+   },
+   scene:{
+      preload,
+      create
+   }
+ }
+
+ 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
