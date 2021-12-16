ecd905ed014ad751bd8f83549881dd84e3d7310c
Timowest12 <timowester@gmail.com>
2021-12-16 19:36:59 +0100

    Add mouse and key click

--- src/index.js
+++ src/index.js
@@ -9,9 +9,8 @@
    physics: {
      default:'arcade',
      arcade:{
-       gravity:{
-         y:200
-       }
+      gravity:{y:200},
+      debug:true
      }
    },
    scene:{
@@ -25,22 +24,26 @@
     this.load.image('bird','assets/bird.png')
  }
 
+ const VELOCITY = 200
  let bird = null;
  let totalDelta = null;
+
  function create(params) {
    this.add.image(0,0,'sky').setOrigin(0,0)
    bird = this.physics.add.sprite(config.width/10,config.height/2,'bird').setOrigin(0.5,0.5)
- }
+  
+   this.input.on('pointerdown',function(){
+    console.log('mouse')
+  })
+   this.input.keyboard.on('keydown_SPACE',function(){
+    console.log('spacebar')
+  })
+  }
 
  
 //60fps
  function update(time, delta){
-  totalDelta += delta
-  //console.log(totalDelta)
-  if (totalDelta < 1000) {return;
-  }
-  console.log(bird.body.velocity.y)
-  totalDelta = 0
+ 
  }
  
 new Phaser.Game(config);
