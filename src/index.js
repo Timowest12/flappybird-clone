36010ec9acd4ed3e590d3cd092fff65b495f5196
Timowest12 <timowester@gmail.com>
2021-12-16 21:39:44 +0100

    Add pipe

--- src/index.js
+++ src/index.js
@@ -22,16 +22,20 @@
  function preload(params) {
     this.load.image('sky','assets/sky.png')
     this.load.image('bird','assets/bird.png')
+    this.load.image('pipe','assets/pipe.png')
  }
 
  const VELOCITY = 200
  let bird = null;
- let flapVelocity = 250
- let totalDelta = null;
+ let pipe = null;
+ const flapVelocity = 250
+ const initialBirdPosition = {x:config.width/10,y:config.height/2}
 
  function create(params) {
    this.add.image(0,0,'sky').setOrigin(0,0)
-   bird = this.physics.add.sprite(config.width/10,config.height/2,'bird').setOrigin(0.5,0.5)
+   //this.add.image(0,0,'pipe').setOrigin(0,0)
+   bird = this.physics.add.sprite(initialBirdPosition.x,initialBirdPosition.y,'bird').setOrigin(0.5,0.5)
+   pipe = this.add.sprite(300,100,'pipe').setOrigin(0,0)
   
    this.input.on('pointerdown',flap)
    this.input.keyboard.on('keydown_SPACE',flap)
@@ -41,8 +45,14 @@
 //60fps
  function update(time, delta){
  if (bird.y > config.height + bird.height|| bird.y  < 0 - bird.height) {
-   alert('you lost!')
+   restartBirdPosition()
  }
+ }
+
+ function restartBirdPosition(){
+  bird.x = initialBirdPosition.x
+  bird.y = initialBirdPosition.y
+  bird.body.velocity.y = 0
  }
 
  function flap() {
